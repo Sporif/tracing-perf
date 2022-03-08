e4a01858f466bfe0083567f0fe1588727c3e8883
Sporif <sporif@posteo.net>
2022-03-08 16:45:26 +0000

    Add configurable `TimeReporterBuilder`
     - add option to set printing order of the total times
     - add option to set formatting width and precision of total times
     - add feature documentation
     - add sample output to README

--- README.md
+++ README.md
@@ -5,3 +5,24 @@ through `tracing`, similar to [slog-perf].
 
 [tracing]: https://github.com/tokio-rs/tracing
 [slog-perf]: https://github.com/slog-rs/perf
+
+## Sample output
+
+```
+2022-02-28 00:14:37.197  INFO time-report: name: File Reader Input, path: 0.004322477, open: 0.138763265, send: 0.607652069
+2022-02-28 00:14:37.258  INFO time-report: name: File Reader Output, recv: 0.039922656, read: 0.726326027, send: 0.044767856
+2022-02-28 00:14:37.276  INFO time-report: name: Chunker, recv: 0.125705828, chunk: 0.346698769, send: 0.348204863
+2022-02-28 00:14:37.277  INFO time-report: name: Chunk Processor, recv: 0.119203928, header: 0.040062816, check: 0.000777103, pack: 0.670504889, write: 0.000077240, send: 0.000154348
+2022-02-28 00:14:37.277  INFO time-report: name: Chunk Processor, recv: 0.144801270, header: 0.040572927, check: 0.000959837, pack: 0.644118678, write: 0.000094198, send: 0.000525979
+2022-02-28 00:14:37.279  INFO time-report: name: Chunk Processor, recv: 0.167694716, header: 0.039238876, check: 0.001081070, pack: 0.624880001, write: 0.000087065, send: 0.000268927
+2022-02-28 00:14:37.282  INFO time-report: name: Chunk Processor, recv: 0.133540186, header: 0.044593754, check: 0.001045541, pack: 0.656200887, write: 0.000088400, send: 0.000389250
+2022-02-28 00:14:37.282  INFO time-report: name: Chunk Processor, recv: 0.127383362, header: 0.041043529, check: 0.000909542, pack: 0.666432821, write: 0.000097267, send: 0.000490198
+2022-02-28 00:14:37.295  INFO time-report: name: Chunk Processor, recv: 0.129950678, header: 0.042644522, check: 0.002428830, pack: 0.674105872, write: 0.000092808, send: 0.000155586
+2022-02-28 00:14:37.296  INFO time-report: name: Entry Processor, recv entry: 0.009082137, process entry: 0.011987756, recv hash: 0.828862962
+2022-02-28 00:14:37.296  INFO time-report: name: Async IO, recv: 0.830499883, read-metadata: 0.000270457, read-metadata send response: 0.000091742
+2022-02-28 00:14:37.296  INFO time-report: name: Async IO, recv: 0.813757364, read: 0.000007788, read send response: 0.000001189, read-metadata: 0.000267161, read-metadata send response: 0.000091131
+2022-02-28 00:14:37.296  INFO time-report: name: Async IO, recv: 0.823099455, read-metadata: 0.000274352, read-metadata send response: 0.000072718
+2022-02-28 00:14:37.296  INFO time-report: name: Async IO, recv: 0.816578158, read-metadata: 0.000280856, read-metadata send response: 0.000089184
+2022-02-28 00:14:37.296  INFO time-report: name: Async IO, recv: 0.808667088, read-metadata: 0.000256612, read-metadata send response: 0.000087649
+2022-02-28 00:14:37.296  INFO time-report: name: Async IO, recv: 0.815601857, read-metadata: 0.000280478, read-metadata send response: 0.000091562
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,120 @@
-//! This crate provides very useful tools for reporting performance metrics
-//! through `tracing`.
-
+#![doc = include_str!("../README.md")]
+#![cfg_attr(
+    feature = "docsrs",
+    cfg_attr(doc, doc = ::document_features::document_features!())
+)]
 #![forbid(unsafe_code)]
 #![warn(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
 
-use std::{collections::HashMap, fmt};
+use std::fmt;
+
+#[cfg(feature = "start-print-order")]
+use indexmap::IndexMap as HashMap;
+#[cfg(not(feature = "start-print-order"))]
+use std::collections::HashMap;
 
 #[cfg(not(feature = "minstant"))]
 use std::time::{Duration, Instant};
 #[cfg(feature = "minstant")]
-use ::{minstant::Instant, std::time::Duration};
+use ::{minstant_crate::Instant, std::time::Duration};
 
 use tracing::Level;
 
-macro_rules! span {
-    ($lvl:expr, $($args:tt)*) => {{
-        match $lvl {
-            Level::ERROR => ::tracing::span!(Level::ERROR, $($args)*),
-            Level::WARN => ::tracing::span!(Level::WARN, $($args)*),
-            Level::INFO => ::tracing::span!(Level::INFO, $($args)*),
-            Level::DEBUG => ::tracing::span!(Level::DEBUG, $($args)*),
-            Level::TRACE => ::tracing::span!(Level::TRACE, $($args)*),
-        }
-    }};
+/// Enum containing possible printing orders of total times.
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[non_exhaustive]
+pub enum PrintOrder {
+    /// Starting order.
+    #[cfg(feature = "start-print-order")]
+    Start,
+    /// Reverse starting order.
+    #[cfg(feature = "start-print-order")]
+    RevStart,
+    /// Key name.
+    Key,
+    /// Reverse key name.
+    RevKey,
+    /// Increasing duration.
+    IncDuration,
+    /// Decreasing duration.
+    DecDuration,
 }
 
-macro_rules! event {
-    (target: $target:expr, $lvl:expr, $($args:tt)*) => {{
-        match $lvl {
-            Level::ERROR => ::tracing::event!(target: $target, Level::ERROR, $($args)*),
-            Level::WARN => ::tracing::event!(target: $target, Level::WARN, $($args)*),
-            Level::INFO => ::tracing::event!(target: $target, Level::INFO, $($args)*),
-            Level::DEBUG => ::tracing::event!(target: $target, Level::DEBUG, $($args)*),
-            Level::TRACE => ::tracing::event!(target: $target, Level::TRACE, $($args)*),
+impl Default for PrintOrder {
+    #[cfg(feature = "start-print-order")]
+    fn default() -> Self {
+        Self::Start
+    }
+    #[cfg(not(feature = "start-print-order"))]
+    fn default() -> Self {
+        Self::DecDuration
+    }
+}
+
+/// A configurable builder for a `TimeReporter`
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct TimeReporterBuilder {
+    name: String,
+    level: Level,
+    print_order: PrintOrder,
+    width: usize,
+    precision: usize,
+}
+
+impl TimeReporterBuilder {
+    /// Create a new `TimeReporter` builder with the given name
+    pub fn new<S: Into<String>>(name: S) -> Self {
+        Self {
+            name: name.into(),
+            level: Level::INFO,
+            print_order: PrintOrder::default(),
+            width: 11,
+            precision: 9,
         }
-    }};
+    }
+
+    #[must_use]
+    pub fn build(&self) -> TimeReporter {
+        TimeReporter {
+            times: HashMap::new(),
+            name: self.name.clone(),
+            cur_state_time: None,
+            level: self.level,
+            print_order: self.print_order,
+            width: self.width,
+            precision: self.precision,
+        }
+    }
+
+    /// Set the logging level.
+    pub fn level(&mut self, level: Level) -> &mut Self {
+        self.level = level;
+        self
+    }
+
+    /// Set the printing order of the total times.
+    pub fn print_order(&mut self, print_order: PrintOrder) -> &mut Self {
+        self.print_order = print_order;
+        self
+    }
+
+    /// Set the minimum formatting width of the total times.
+    ///
+    /// Note: Should be at least `precision + 2`  (i.e at least one leading digit + decimal point + precision)
+    /// or this option will have no effect. Alternatively precision should be at most `width - 2`.
+    ///
+    /// Fill character and alignment are hardcoded to space and left-align.
+    pub fn width(&mut self, width: usize) -> &mut Self {
+        self.width = width;
+        self
+    }
+
+    /// Set the number of digits after the decimal point
+    /// that should be printed for the total times.
+    pub fn precision(&mut self, precision: usize) -> &mut Self {
+        self.precision = precision;
+        self
+    }
 }
 
 /// Collect and report total time spent on set of activities.
@@ -50,27 +130,20 @@ pub struct TimeReporter {
     cur_state_time: Option<(&'static str, Instant)>,
     name: String,
     level: Level,
+    print_order: PrintOrder,
+    width: usize,
+    precision: usize,
 }
 
 impl TimeReporter {
-    /// Create new `TimeReporter`.
+    /// Create a new `TimeReporter`.
     pub fn new<S: Into<String>>(name: S) -> Self {
-        Self {
-            times: HashMap::new(),
-            name: name.into(),
-            cur_state_time: None,
-            level: Level::INFO,
-        }
+        TimeReporterBuilder::new(name).build()
     }
 
-    /// Create new `TimeReporter` with a specified level.
+    /// Create a new `TimeReporter` with a specified level.
     pub fn new_with_level<S: Into<String>>(name: S, level: Level) -> Self {
-        Self {
-            times: HashMap::new(),
-            name: name.into(),
-            cur_state_time: None,
-            level,
-        }
+        TimeReporterBuilder::new(name).level(level).build()
     }
 
     /// Start counting time for a state named "key".
@@ -80,9 +153,7 @@ impl TimeReporter {
     /// before starting a new one.
     pub fn start(&mut self, key: &'static str) {
         let now = Instant::now();
-
         self.save_current(now);
-
         self.cur_state_time = Some((key, now));
     }
 
@@ -116,30 +187,76 @@ impl TimeReporter {
     pub fn finish(self) {}
 }
 
+fn get_times(
+    times: &HashMap<&'static str, Duration>,
+    print_order: PrintOrder,
+) -> Vec<(&'static str, Duration)> {
+    cfg_if::cfg_if! {
+        if #[cfg(feature = "start-print-order")] {
+            if print_order == PrintOrder::RevStart {
+                times.iter().rev().map(|(&k, &v)| (k, v)).collect()
+            } else {
+                times.iter().map(|(&k, &v)| (k, v)).collect()
+            }
+        } else {
+            let _ = print_order;
+            times.iter().map(|(&k, &v)| (k, v)).collect()
+        }
+    }
+}
+
 impl<'a> fmt::Display for TimeReporter {
-    #[allow(clippy::cast_precision_loss, clippy::cast_lossless)]
+    #[allow(clippy::cast_precision_loss)]
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut stats: Vec<(&'static str, Duration)> =
-            self.times.iter().map(|(&k, &v)| (k, v)).collect();
-        stats.sort_by_key(|s| s.1);
+        let mut stats: Vec<(&'static str, Duration)> = get_times(&self.times, self.print_order);
+        match self.print_order {
+            #[cfg(feature = "start-print-order")]
+            PrintOrder::Start | PrintOrder::RevStart => {}
+            PrintOrder::Key => stats.sort_by_key(|s| s.0),
+            PrintOrder::RevKey => stats.sort_by(|a, b| b.0.cmp(a.0)),
+            PrintOrder::IncDuration => stats.sort_by_key(|s| s.1),
+            PrintOrder::DecDuration => stats.sort_by(|a, b| b.1.cmp(&a.1)),
+        }
 
         write!(f, "name: {}", self.name)?;
-        for &(state, dur) in stats.iter().rev() {
-            write!(
-                f,
-                ", {}: {}",
-                state,
-                dur.as_secs() as f64 + dur.subsec_nanos() as f64 / 1_000_000_000_f64
-            )?;
+        let precision = self.precision;
+        let width = self.width;
+        for &(state, dur) in &stats {
+            let dur = dur.as_secs() as f64 + f64::from(dur.subsec_nanos()) / 1_000_000_000_f64;
+            write!(f, ", {}: {:<width$.precision$}", state, dur)?;
         }
 
         Ok(())
     }
 }
 
+macro_rules! _span {
+    ($lvl:expr, $($args:tt)*) => {{
+        match $lvl {
+            Level::ERROR => ::tracing::span!(Level::ERROR, $($args)*),
+            Level::WARN  => ::tracing::span!(Level::WARN,  $($args)*),
+            Level::INFO  => ::tracing::span!(Level::INFO,  $($args)*),
+            Level::DEBUG => ::tracing::span!(Level::DEBUG, $($args)*),
+            Level::TRACE => ::tracing::span!(Level::TRACE, $($args)*),
+        }
+    }};
+}
+
+macro_rules! _event {
+    (target: $target:expr, $lvl:expr, $($args:tt)*) => {{
+        match $lvl {
+            Level::ERROR => ::tracing::event!(target: $target, Level::ERROR, $($args)*),
+            Level::WARN  => ::tracing::event!(target: $target, Level::WARN,  $($args)*),
+            Level::INFO  => ::tracing::event!(target: $target, Level::INFO,  $($args)*),
+            Level::DEBUG => ::tracing::event!(target: $target, Level::DEBUG, $($args)*),
+            Level::TRACE => ::tracing::event!(target: $target, Level::TRACE, $($args)*),
+        }
+    }};
+}
+
 impl Drop for TimeReporter {
     fn drop(&mut self) {
-        let _span = span!(self.level, "time-report").entered();
-        event!(target: "tracing-perf", self.level, "{}", self);
+        let _span = _span!(self.level, "time-report").entered();
+        _event!(target: "tracing-perf", self.level, "{}", self);
     }
 }
